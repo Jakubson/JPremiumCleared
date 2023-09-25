b88b0c9318aaa9b982dadaae48392fa9dfa47198
Jakubson <19249808+Jakubson@users.noreply.github.com>
2023-09-25 08:21:09 +0200

    Add files via upload

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jakub.jpremium.velocity.api"},{"l":"com.jakub.jpremium.velocity.api.event"},{"l":"com.jakub.premium.api"},{"l":"com.jakub.premium.api.event"}];updateSearchResults();
